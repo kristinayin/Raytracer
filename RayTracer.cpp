1f97d10b4fc1567ba54e9a05502b1bfc3bec83c4
Patrick <patricklee260@gmail.com>
2021-10-21 15:58:37 -0700

    ideas we had about render function

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -4,6 +4,14 @@ void RayTracer::clear() const {//what does this func do?
 }
 
 void RayTracer::render(const Scene& _scene) const {
+
+  for(int i = 0; i<length; i++){//length is # of col
+    for(int j = 0; j<height; j++){//height is # of rows
+      //using ray struct that takes in some origin and direction
+      //using collision function for the sphere
+      //use material struct???
+    }
+  }
   // for each column i do
   //   for each row j do
   //     generate a ray origin and direction
