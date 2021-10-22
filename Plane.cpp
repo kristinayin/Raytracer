09ab9fbdad1337cf6638a02ecf7f55225e01871d
Kristina Yin <kristinacyin@gmail.com>
2021-10-22 13:54:55 -0700

    updated plane class

--- Plane.cpp
+++ Plane.cpp
@@ -5,7 +5,22 @@
 //  Created by Kristina Yin on 10/22/21.
 //
 
+#include <vector>
+
+using namespace std;
+using namespace glm;
 
 class Plane {
-    
+public:
+    Plane() {
+        point = vec3(0,0,0);
+        normal = vec3(0,0,0);
+    }
+    Plane(int y, vec3 place) {
+        point = place;
+        normal = vec3(0,y,0);
+    }
+private:
+    vec3 point;
+    vec3 normal;
 }
