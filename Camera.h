afa364a136b510182c92303f61f99928fef2c242
Patrick <patricklee260@gmail.com>
2021-10-24 15:06:18 -0700

    added a camera structure

--- Camera.h
+++ Camera.h
@@ -3,9 +3,11 @@
 
 // GL
 #include "GLInclude.h"
+//#include "Ray.h" //not sure if we need this
 
-class Camera {
-
+struct Camera {
+    Camera(const glm::vec3& _eye) : _eye(0, 0, 0);
+    float focal = 1.0;//focal length from eye to view frame
 };
 
 #endif
