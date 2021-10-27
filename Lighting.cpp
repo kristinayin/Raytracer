bcbcd2ed7c1084eef101bfa2b8e4c352110a3dbd
LIL-CREAMY <91923096+LIL-CREAMY@users.noreply.github.com>
2021-10-26 17:14:42 -0700

    a whole heap of lighting stuff

--- Lighting.cpp
+++ Lighting.cpp
@@ -1,2 +1,13 @@
 #include "Lighting.h"
+#include "Plane.h"
+#include "Sphere.h"
 
+glm::vec3 calculateDirection(const glm::vec3& p,const glm::vec3& x){
+    glm::vec3 n, d, r;
+    n= p-x;
+    d= p-x;
+    r=d/glm::length(d);
+    return r;
+}
+
+//glm::vec3
--- Lighting.h
+++ Lighting.h
@@ -4,8 +4,35 @@
 #include "GLInclude.h"
 #include <vector>
 
-class Light(glm::vec3 p, glm::vec3 c){
-    glm::vec3 point
-    
+class Light: public Object{
+    //public:
+        glm::vec3 point;
+        //light intensities for Ambient diffuse & specuar components
+        glm::vec3 intensities;
+        glm::vec3 direction;
+        float theta;//cut off angle
+        glm::vec3 LAC;//linearattenuation constants
+        float attenuation;
 
-}
+        //point light
+        Light(const glm::vec3& p, const glm::vec3& color, const glm::vec3& attenconst){
+             point = p;
+             intensities = color;
+             LAC = attenconst;
+        }
+        //spotlight
+        Light(const glm::vec3& p, const glm::vec3& color,const glm::vec3& attenconst, glm::vec3 d, float t, float a){
+            point = p;
+            intensities = color;
+            LAC = attenconst;
+            direction = d;
+            theta = t;
+            attenuation = a;
+
+
+
+        }
+  
+};
+
+#endif
