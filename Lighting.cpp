3b71008d98528cc47a99703d3f03dcde708005d5
LIL-CREAMY <91923096+LIL-CREAMY@users.noreply.github.com>
2021-10-31 19:50:42 -0700

    small changes and added a function declaration

--- Lighting.cpp
+++ Lighting.cpp
@@ -1,13 +1,21 @@
 #include "Lighting.h"
 #include "Plane.h"
 #include "Sphere.h"
+#include "Scene.h"
 
-glm::vec3 calculateDirection(const glm::vec3& p,const glm::vec3& x){
+//calculates direction to light from a point x
+glm::vec3 calculateDirection(const glm::vec3& p, const glm::vec3& x){
     glm::vec3 n, d, r;
+    //numerator
     n= p-x;
+    //denominator
     d= p-x;
+
+    //result
     r=d/glm::length(d);
     return r;
 }
 
-//glm::vec3
+glm::vec3 lambertianShading(glm::vec3 kd, glm::vec3 id, Object o){
+
+}
