3cf4a9bb33094ebc08f4319aadfd82c978dc8347
Patrick <patricklee260@gmail.com>
2021-11-07 15:08:32 -0800

    getting of more comments we don't need

--- Collision.h
+++ Collision.h
@@ -31,17 +31,4 @@ struct Collision {
   const Material* m_material; ///< Material of surface
 };
 
-/*Collision planeIntersection(Ray i, Plane j){
-  if(glm::dot(i._d, j.n)>0){//if vector i dot normal greater than 0
-    float t = glm::dot((j.p-i._o), j.n)/glm::dot(i._d, j.n);
-    if(t>0){
-      Collision result(i._o + t*i._d, plane.n,there needs to be a material here);//result is a ray with the point of collision and the normal of the plane
-      kHit = 1;
-      return result;
-    } 
-  }
-  return 0;
-  //return something else??
-}
-*/
 #endif
