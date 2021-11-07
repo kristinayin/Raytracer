73b89d21a47db732fe4dadac30003a8c176fe0d7
Patrick <patricklee260@gmail.com>
2021-11-07 15:09:10 -0800

    getting rid of more comments

--- Plane.cpp
+++ Plane.cpp
@@ -26,32 +26,3 @@ Collision Plane:: collide(const Ray& _ray) const {
 
     return Collision();
 }
-
-//using namespace std;
-//using namespace glm;
-
-
-/*
-class Plane {
-public:
-//private: //making this private because its causing an out of scope error
-    glm::vec3 point;
-    glm::vec3 normal;
-    Plane() {
-        point = glm::vec3(0,0,0);
-        normal = glm::vec3(0,0,0);
-    }
-    
-    
-    Better way to set up constructors?
-    Plane() : point(vec3(0,0,0)), normal(vec3(0,0,0)) {}
-    ...something like that
-    
-    
-    Plane(int y, glm::vec3 place) {
-        point = place;
-        normal = glm::vec3(0,y,0);
-    }
-
-};
-*/
