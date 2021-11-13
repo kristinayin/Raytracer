be7dd243fe51893a1ed7270cec87c016de12859e
Patrick <patricklee260@gmail.com>
2021-11-13 09:19:58 -0800

    getting rid of comments

--- Plane.h
+++ Plane.h
@@ -21,7 +21,7 @@ class Plane : public Object {
 
     glm::vec3 getNormal();
 
-  private://making this private because its causing an out of scope error?
+  private:
     glm::vec3 point, normal;//point on the plane and plane's normal
     Material m_material;//plane material find out how to make this private
 };
