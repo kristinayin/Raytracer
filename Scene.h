75226a2a8e8179378f6f99ff9834a65b7fe96859
Patrick <patricklee260@gmail.com>
2021-11-18 15:14:04 -0800

    vector of lights should not be a pointer

--- Scene.h
+++ Scene.h
@@ -8,7 +8,7 @@ class Scene {
   public:
     Scene(){}
     void addObject(Object *_obj);
-    void addLight(Light * lt);
+    void addLight(Light lt);
     //making objects vector public for now
 
     std::vector<Object*> getObj(){
@@ -18,7 +18,7 @@ class Scene {
     std::vector<Light> getLights(){
       return lights;
     }
-    
+
   private:
     std::vector<Object*> objects;
     std::vector<Light> lights;//adding a light vector
